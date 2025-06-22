18c86e3d3942e11eaee8d9d60c7e99e0237a53ec
Avtar <ratva@users.noreply.github.com>
2025-06-22 03:09:23 -0400

    fix: Mock Neo4j driver in tests to pass CI

--- bookmarked-api/src/test/java/com/bookmarked/bookmarked_api/BookmarkedApiApplicationTests.java
+++ bookmarked-api/src/test/java/com/bookmarked/bookmarked_api/BookmarkedApiApplicationTests.java
@@ -1,7 +1,9 @@
 package com.bookmarked.bookmarked_api;
 
 import org.junit.jupiter.api.Test;
+import org.neo4j.driver.Driver;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class BookmarkedApiApplicationTests {
