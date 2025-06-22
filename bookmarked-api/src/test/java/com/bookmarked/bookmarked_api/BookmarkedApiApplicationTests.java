16fc4ca415985c28459ef6645c8dc533091f760f
Avtar <ratva@users.noreply.github.com>
2025-06-22 02:53:50 -0400

    feat: Initialize Spring Boot project and add configurations

---
+++ bookmarked-api/src/test/java/com/bookmarked/bookmarked_api/BookmarkedApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookmarked.bookmarked_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmarkedApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
